85ad41c803d62bb37099785fc582642542bff2cf
Gustavo B Barska <gbarska@yahoo.com>
2019-10-06 12:44:41 -0300

    robotToOrigin

---
+++ robotToOrigin/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace robotToOrigin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(judgeCircle("LRUD"));
+        }
+
+        public static bool judgeCircle(string moves){
+            int UD = 0;
+            int LR = 0;
+
+            for (int i = 0; i < moves.Length; i++)
+            {
+                char currentMove = moves[i];
+                if (currentMove == 'U'){
+                    UD++;
+                } else if(currentMove == 'D'){
+                    UD--;
+                }else if(currentMove == 'L'){
+                    LR++;
+                }else if(currentMove == 'R'){
+                    LR--;
+                }
+
+            }
+
+            return UD == 0 && LR == 0;
+        }
+    }
+}
